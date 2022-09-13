9296ca3bee61126459bd49b2b222c39573abbfea
Micah Pearlman <micah@zeroengineering.io>
2022-09-13 12:14:09 -0700

    working mouse

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,11 +2,11 @@
 #include <imgui.h>
 #include <bgfx/bgfx.h>
 #include <glm/gtx/euler_angles.hpp>
-
+#include <iostream>
 namespace bigger {
 class CameraImpl final : public Camera {
   public:
-    CameraImpl(const std::string& name, int view_width, int view_height)
+    CameraImpl(const std::string &name, int view_width, int view_height)
         : Camera(name), _view_width(view_width), _view_height(view_height) {}
 
     glm::mat4 viewMatrix() const override { return transform(); }
@@ -42,22 +42,54 @@ class CameraImpl final : public Camera {
         {
 
             ImGui::Text("Camera Setting");
-            float roll = 0, pitch = 0, yaw = 0;
-            glm::extractEulerAngleYXZ(rotationMatrix(), yaw, pitch, roll);
+   
+            // check if we are dragging the mouse with left button down outside
+            // any window
+            if (ImGui::IsMouseDragging(ImGuiMouseButton_Left) == true &&
+                ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow) == false) {
+                _is_mouse_look = true;    
+                ImVec2 mouse_delta =
+                    ImGui::GetMouseDragDelta(ImGuiMouseButton_Left);
+                ImVec2 delta = {mouse_delta.x - _last_mouse_delta.x,
+                                mouse_delta.y - _last_mouse_delta.y};
+                _last_mouse_delta = mouse_delta;
 
-            // ImGui::SliderFloat3("Position", glm::value_ptr(_position), -5.0f,
-            //                     5.0f);
-            // ImGui::SliderFloat3("Target", glm::value_ptr(_target),
-            // -2.0f, 2.0f);
+                float normalized_x = (float)delta.x / (float)_view_width;
+                float normalized_y = (float)delta.y / (float)_view_height;
+                _yaw += (normalized_x * _mouse_speed);
+                _pitch += (normalized_y * _mouse_speed);
 
-            ImGui::SliderFloat("Roll", &roll, -glm::pi<float>(),
+            } else {
+                if (_is_mouse_look == true) {
+                    _last_mouse_delta = {0,0};
+                }
+                _is_mouse_look = false;
+            }
+
+            ImGui::SliderFloat("Roll", &_roll, -glm::pi<float>(),
                                glm::pi<float>());
-            ImGui::SliderFloat("Pitch", &pitch, -glm::pi<float>(),
-                               glm::pi<float>());
-            ImGui::SliderFloat("Yaw", &yaw, -glm::pi<float>(),
+            ImGui::SliderFloat("Pitch", &_pitch, -glm::pi<float>() * 0.5f,
+                               glm::pi<float>() * 0.5f);
+            ImGui::SliderFloat("Yaw", &_yaw, -glm::pi<float>(),
                                glm::pi<float>());
 
-            glm::mat4 rotation = glm::eulerAngleYXZ(yaw, pitch, roll);
+            if (ImGui::Button("Reset") == true) {
+                _pitch = _yaw = 0;
+            }                   
+
+            _yaw = glm::clamp(_yaw, -glm::pi<float>(), glm::pi<float>());
+            _pitch = glm::clamp(_pitch, -glm::pi<float>() * 0.5f,
+                                glm::pi<float>() * 0.5f);
+            _roll = glm::clamp(_roll, -glm::pi<float>(), glm::pi<float>());
+
+            glm::mat4 yaw_mat = glm::rotate(glm::mat4(1), _yaw, {0,1,0});   
+            glm::vec3 right = glm::inverse(yaw_mat) * glm::vec4(1,0,0,0);
+            glm::mat4 pitch_mat = glm::rotate(glm::mat4(1), _pitch, right);
+            glm::mat4 rotation = yaw_mat * pitch_mat; 
+            glm::vec3 forward = glm::inverse(rotation) * glm::vec4(0,0,1,0);
+            glm::mat4 roll_mat = glm::rotate(glm::mat4(1), _roll, forward);
+            rotation = rotation * roll_mat;
+
             setRotationMatrix(rotation);
 
             ImGui::SliderFloat("FoV (Degree)", &_fov, 10.0f, 120.0f);
@@ -70,19 +102,24 @@ class CameraImpl final : public Camera {
     void destroy() override {}
 
   private:
-    // glm::vec3 _position = glm::vec3(1.0f, 1.0f, -2.0f);
-    // glm::vec3 _target = glm::vec3(0.0f, 0.0f, 0.0f);
-    // glm::vec3 _up = glm::vec3(0.0f, 1.0f, 0.0f);
-
     float _fov = 60.0f;
     float _near_clip = 0.1f;
     float _far_clip = 100.0f;
 
     int _view_width = 0;
     int _view_height = 0;
+
+    float _roll = 0;
+    float _pitch = 0;
+    float _yaw = 0;
+
+    ImVec2 _last_mouse_delta = {0, 0};
+    float  _mouse_speed = 8.5f;
+    bool _is_mouse_look = false;
 };
 
-std::shared_ptr<Camera> Camera::create(const std::string& name, int view_width, int view_height) {
+std::shared_ptr<Camera> Camera::create(const std::string &name, int view_width,
+                                       int view_height) {
     return std::make_shared<CameraImpl>(name, view_width, view_height);
 }
 
